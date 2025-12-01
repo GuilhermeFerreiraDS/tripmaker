96b5c809efea3d986940501387c34697ccc083b2
joaobragheroli <joao.bragheroli@gmail.com>
2025-12-01 14:47:13 -0300

    novidade

--- Home/index.php
+++ Home/index.php
@@ -1,153 +1,48 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>TripMaker</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TripMaker</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
+
 <body>
-  <!-- Cabeçalho -->
-  <header class="top-bar">
-    <div class="logo-icons">
-      <i class="fa-solid fa-play logo"></i>
-      <i class="fa-regular fa-circle-user user"></i>
-    </div>
-    <div class="search-box">
-      <input type="text" placeholder="Digite seu destino...">
-      <i class="fa-solid fa-magnifying-glass"></i>
-    </div>
-  </header>
-
-  <div class="slider">
-
-
-<div class="list">
-
-    <div class="item">
-        <img src="./img/sorocaba.jpeg" alt="">
-
-        <div class="content">
-            <div class="title">SOROCABA</div>
-            <div class="type">FLOWER</div>
-            <div class="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
-            </div>
-            <div class="button">
-                <button>SEE MORE</button>
-            </div>
-        </div>
-    </div>
-
-    <div class="item">
-        <img src="./img/itu.webp" alt="">
-
-        <div class="content">
-            <div class="title">ITU</div>
-            <div class="type">MAEDA</div>
-            <div class="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
-            </div>
-            <div class="button">
-                <button>SEE MORE</button>
-            </div>
-        </div>
-    </div>
-
-    <div class="item">
-        <img src="./img/tatui.jpg" alt="">
-
-        <div class="content">
-            <div class="title">TATUÍ</div>
-            <div class="type">PRAÇA DA MATRIZ</div>
-            <div class="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
-            </div>
-            <div class="button">
-                <button>SEE MORE</button>
-            </div>
-        </div>
-    </div>
-
-    <div class="item">
-        <img src="./img/boituva.jpg" alt="">
-
-        <div class="content">
-            <div class="title">BOITUVA</div>
-            <div class="type">NATURE</div>
-            <div class="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
-            </div>
-            <div class="button">
-                <button>SEE MORE</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<div class="thumbnail">
-
-    <div class="item">
-        <img src="./img/sorocaba.jpeg" alt="">
-    </div>
-    <div class="item">
-        <img src="./img/itu.webp" alt="">
-    </div>
-    <div class="item">
-        <img src="./img/tatui.jpg" alt="">
-    </div>
-    <div class="item">
-        <img src="./img/boituva.jpg" alt="">
-    </div>
-
-</div>
-
-<div class="nextPrevArrows">
-                    <button class="prev"> < </button>
-                    <button class="next"> > </button>
-                </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <!-- Slider com container de largura total (fora da main) -->
-  <!-- <div class="slider-fullwidth">
+    <!-- Conteiner -->
     <div class="slider">
+        <!-- Container Carrossel -->
         <div class="list">
+            <!-- Cabeçalho -->
+            <header class="top-bar">
+                <div class="logo-icons">
+                    <div class="icone">
+                        <i class="fa-solid fa-play logo"></i>
+                    </div>
+                    <div class="paginas">
+                        <a href="../Anuncie_Espaco/1.Ambiente/">Anuncie seu Espaço</a>
+                        <a href="../Anuncie_Espaco/1.Ambiente/">Minhas Viagens</a>
+                        <a href="../Anuncie_Espaco/1.Ambiente/">Pagina IA</a>
+                    </div>
+
+                    <div class="login_icone">
+                        <i class="fa-regular fa-circle-user user"></i>
+                    </div>
+                    
+                </div>
+                <!-- <div class="search-box">
+                    <input type="text" placeholder="Digite seu destino...">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div> -->
+            </header>
+
+            <!-- Carrosel Img Grande  -->
             <div class="item">
-                <img src="img1.jpg" alt="">
+                <img src="./img/sorocaba.jpeg" alt="">
+
                 <div class="content">
-                    <div class="title">MAGIC SLIDER</div>
+                    <div class="title">SOROCABA</div>
                     <div class="type">FLOWER</div>
                     <div class="description">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
@@ -159,10 +54,11 @@
             </div>
 
             <div class="item">
-                <img src="img2.jpg" alt="">
+                <img src="./img/itu.webp" alt="">
+
                 <div class="content">
-                    <div class="title">MAGIC SLIDER</div>
-                    <div class="type">NATURE</div>
+                    <div class="title">ITU</div>
+                    <div class="type">MAEDA</div>
                     <div class="description">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
                     </div>
@@ -173,10 +69,11 @@
             </div>
 
             <div class="item">
-                <img src="img4.jpg" alt="">
+                <img src="./img/tatui.jpg" alt="">
+
                 <div class="content">
-                    <div class="title">MAGIC SLIDER</div>
-                    <div class="type">PLANT</div>
+                    <div class="title">TATUÍ</div>
+                    <div class="type">PRAÇA DA MATRIZ</div>
                     <div class="description">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
                     </div>
@@ -187,9 +84,10 @@
             </div>
 
             <div class="item">
-                <img src="img3.jpg" alt="">
+                <img src="./img/boituva.jpg" alt="">
+
                 <div class="content">
-                    <div class="title">MAGIC SLIDER</div>
+                    <div class="title">BOITUVA</div>
                     <div class="type">NATURE</div>
                     <div class="description">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis eum consequuntur voluptate quae doloribus distinctio. Possimus, sed recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aut.
@@ -201,67 +99,71 @@
             </div>
         </div>
 
+        <!-- Carrossel de Cards -->
         <div class="thumbnail">
             <div class="item">
-                <img src="img1.jpg" alt="">
+                <img src="./img/boituva.jpg" alt="">
+            </div>
+
+            <div class="item">
+                <img src="./img/tatui.jpg" alt="">
             </div>
             <div class="item">
-                <img src="img2.jpg" alt="">
+                <img src="./img/itu.webp" alt="">
             </div>
+
             <div class="item">
-                <img src="img4.jpg" alt="">
-            </div>
-            <div class="item">
-                <img src="img3.jpg" alt="">
+                <img src="./img/sorocaba.jpeg" alt="">
             </div>
         </div>
 
         <div class="nextPrevArrows">
-            <button class="prev"> < </button>
-            <button class="next"> > </button>
+            <button class="prev">
+                < </button>
+                    <button class="next"> > </button>
         </div>
     </div>
-  </div> -->
 
-  <!-- Conteúdo principal -->
-  <main class="container">
-    <h1 class="brand">TripMaker</h1>
+    <!-- Conteúdo principal -->
+    <main class="container">
+        <h1 class="brand">TripMaker</h1>
 
-    <a href="#" class="highlight-card">
-      <p>Confira Pacotes por Acomodação</p>
-      <img src="https://picsum.photos/200/100?random=2" alt="Pacote">
-    </a>
+        <a href="#" class="highlight-card">
+            <p>Confira Pacotes por Acomodação</p>
+            <img src="https://picsum.photos/200/100?random=2" alt="Pacote">
+        </a>
 
-    <!-- Hospedagem -->
-    <section class="section">
-      <h2>🏨 Hospedagem</h2>
-      <div class="grid">
-        <img src="https://picsum.photos/100?random=3" alt="hotel">
-        <img src="https://picsum.photos/100?random=4" alt="hotel">
-        <img src="https://picsum.photos/100?random=5" alt="hotel">
-      </div>
-    </section>
+        <!-- Hospedagem -->
+        <section class="section">
+            <h2>🏨 Hospedagem</h2>
+            <div class="grid">
+                <img src="https://picsum.photos/100?random=3" alt="hotel">
+                <img src="https://picsum.photos/100?random=4" alt="hotel">
+                <img src="https://picsum.photos/100?random=5" alt="hotel">
+            </div>
+        </section>
 
-    <!-- Pontos Turísticos -->
-    <section class="section">
-      <h2>📍 Pontos Turísticos</h2>
-      <div class="grid">
-        <img src="https://picsum.photos/100?random=6" alt="ponto turístico">
-        <img src="https://picsum.photos/100?random=7" alt="ponto turístico">
-        <img src="https://picsum.photos/100?random=8" alt="ponto turístico">
-      </div>
-    </section>
+        <!-- Pontos Turísticos -->
+        <section class="section">
+            <h2>📍 Pontos Turísticos</h2>
+            <div class="grid">
+                <img src="https://picsum.photos/100?random=6" alt="ponto turístico">
+                <img src="https://picsum.photos/100?random=7" alt="ponto turístico">
+                <img src="https://picsum.photos/100?random=8" alt="ponto turístico">
+            </div>
+        </section>
 
-    <!-- Mais Visitados -->
-    <section class="section">
-      <h2>🔥 Mais Visitados</h2>
-      <div class="grid">
-        <img src="https://picsum.photos/100?random=9" alt="popular">
-        <img src="https://picsum.photos/100?random=10" alt="popular">
-        <img src="https://picsum.photos/100?random=11" alt="popular">
-      </div>
-    </section>
-  </main>
-  <script src="app.js"></script>
+        <!-- Mais Visitados -->
+        <section class="section">
+            <h2>🔥 Mais Visitados</h2>
+            <div class="grid">
+                <img src="https://picsum.photos/100?random=9" alt="popular">
+                <img src="https://picsum.photos/100?random=10" alt="popular">
+                <img src="https://picsum.photos/100?random=11" alt="popular">
+            </div>
+        </section>
+    </main>
+    <script src="app.js"></script>
 </body>
+
 </html>
